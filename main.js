4efc58ff648ca7ea71deba5a46137ad80961817c
R-Newson <yearnnewsonogweno@gmail.com>
2024-07-11 14:59:36 +0300

    Created a portfolio using HTML, CSS, JS

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Backend Developer", "Project Manager", "Web Developer", "Clinical Officer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
